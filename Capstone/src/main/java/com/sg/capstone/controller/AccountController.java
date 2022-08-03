0e5a6921fa860349b75ddc5e91a40a00b438d502
Alain Weng <alain.weng@gmail.com>
2022-08-03 00:35:49 -0400

    added /login page and keeping track of the username of the person logged in

---
+++ Capstone/src/main/java/com/sg/capstone/controller/AccountController.java
@@ -0,0 +1,57 @@
+package com.sg.capstone.controller;
+
+import com.sg.capstone.dao.AuthorDAO;
+import com.sg.capstone.dao.BlogDAO;
+import com.sg.capstone.dao.HashtagDao;
+import com.sg.capstone.model.Author;
+import com.sg.capstone.model.Blog;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@Controller
+public class AccountController {
+
+	@Autowired
+	BlogDAO blogDao;
+	@Autowired 
+	AuthorDAO authorDao;
+	
+	@GetMapping("/login")
+    public String loginScreen(Model model) {
+		//model.addAttribute("username","asdf");
+        return "login";
+    }
+	
+	@PostMapping("/login")
+	public String login(Model model, HttpServletRequest request) {
+		String username = request.getParameter("name");
+		String password = request.getParameter("password");
+		Author author = authorDao.getAuthor(username);
+		if(author!=null && author.getPass().equals(password)){
+			model.addAttribute("username",username);
+		}
+		return "/login";
+	}
+	
+	@GetMapping("/Home")
+    public String displayHome(Model model){
+		Blog blog = blogDao.getLatestBlog();
+		model.addAttribute("blog", blog);
+        return "Home";
+    }
+
+}
